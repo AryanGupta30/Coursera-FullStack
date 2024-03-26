42201160435a4bf8a867ca08595687ba3de50cf5
Aryan Gupta <30aryang@gmail.com>
2024-03-26 09:47:08 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
 import { IoIosSearch } from "react-icons/io";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './App.css';
+import { Button } from 'react-bootstrap';
 
 export default function App() {
   return (
@@ -14,6 +19,13 @@ export default function App() {
         <div className='icon'>
           <IoIosSearch color='white' />
         </div>
+        <div className='nav2'>
+          <Button className='opt'>Online Degrees</Button>
+          <Button className='opt'>Find new careers</Button>
+          <Button className='opt'>Log in </Button>
+          <Button>Join for free</Button>
+
+        </div>
 
       </div>
     </Container>
@@ -21,44 +33,5 @@ export default function App() {
   )
 }
 
-const Container = styled.div`
-  .nav {
-    display: flex;
-    align-items: center;
-    padding: 1rem;
-    background-color: white;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  button {
-    padding: 0.5rem 1rem;
-    background-color: blue;
-    border-radius: 0.5rem;
-    border-width: 1.5px;
-    cursor: pointer;
-    font-size: 1rem;
-    color: white;
-    font-weight: 500;
-    transition: 0.3s;
-    margin-right: 6px;
-  }
-  button:hover {
-    background-color: #f0f0f0;
-  }
-  input {
-    padding: 0.5rem 1rem;
-    font-size: 1rem;
-    color: #333;
-    font-weight: 500;
-    background-color: #f0f0f0;
-    border-width: 1px;
-  }
-  .icon{
-    background-color: blue;
-    border-radius: 0.1rem;
-    padding: 0.5rem 0.8rem;
-    margin-right: 6px;
-  }
-  
-`
 
 

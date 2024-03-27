4b9e1b9b340ab6a6b8df1481df39320270bbd85b
Aryan Gupta <30aryang@gmail.com>
2024-03-27 23:50:29 +0530

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,37 @@ import imperial from '../assets/imperial.png'
 import penn from '../assets/penn.png'
 import stanford from '../assets/stanford.png'
 import umich from '../assets/umich.png'
+import { IoIosSearch } from "react-icons/io";
+
 
 
 const Home = () => {
+
+    let items = [
+        {
+            img : require('../assets/learn.png'),
+            heading: 'Learn Anything',
+            para:'Explore any interest or trending topic, take prerequisites, and advance your skills'
+        },
+        {
+            img : require('../assets/money.png'),
+            heading: 'Save money',
+            para:'Explore any interest or trending topic, take prerequisites, and advance your skills'
+        },
+        {
+            img : require('../assets/flexible.png'),
+            heading: 'Flexible learning',
+            para:'Explore any interest or trending topic, take prerequisites, and advance your skills'
+        },
+        {
+            img : require('../assets/certificates.png'),
+            heading: 'Unlimited Certificates',
+            para:'Explore any interest or trending topic, take prerequisites, and advance your skills'
+        },
+
+    ];
+
+    let fields = ['POPULAR','Business','Computer Science','Data Science','Health','Information Technology','Arts and Humanities']
   return (
     <>
     <div className='main-div' style={{ display: 'flex', justifyContent: 'space-between', marginLeft: '80px', marginBottom: '85px'}}>
@@ -33,7 +61,7 @@ const Home = () => {
         </div>
     </div>
     <div className='image-row' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#f5f5f5'}}>
-        <div style={{ margin: '20px', fontSize: '0.8rem' }}>
+        <div style={{ margin: '20px', fontSize: '0.8rem',fontWeight:'normal' }}>
             <h1>We collaborate with <span style={{color: '#0056d2'}}>325+ leading universities and companies</span></h1>
         </div>
 
@@ -47,6 +75,44 @@ const Home = () => {
             <img src={stanford} alt="Stanford" style={{ marginRight: '10px', width: '135px', height: '50px', marginTop: '7px'}} />
             <img src={penn} alt="Penn" style={{ marginRight: '10px', width: '150px', height: '60px'}} />
             
+        </div>
+    </div>
+    <div className='benefits' style={{ display: 'flex', flexDirection: 'column',justifyContent:'space-between'}}>
+        <div style={{ fontSize: '1rem',marginLeft:'350px'}}>
+            <h1 style={{fontWeight:'500'}}>
+                Invest in your career with Coursera
+            </h1>
+        </div>
+            <p style={{fontSize:'1rem',marginLeft:'250px',marginRight:'250px',alignContent:'center'}}>
+            Get access to videos in over 90% of courses, Specializations, and Professional Certificates taught by top instructors from leading universities and companies.
+            </p>
+        <div className='img-cards' style={{display:'flex',flexDirection:'row',marginLeft:'50px',marginRight:'20px'}}>
+            {items.map((item, index) => (
+                    <div key={index} style={{display:'flex',flexDirection:'column',alignItems:'center',marginTop:'30px'}}>
+                        <img src={item.img} alt="Item Image" style={{width:'100px',height:'80px'}}/>
+                        <h3 style={{fontWeight:'400'}}>{item.heading}</h3>
+                        <p style={{fontSize:'1rem',alignContent:'flex-end',marginRight:'5px'}}>{item.para}</p>
+                    </div>
+                ))}
+        </div>
+    </div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',display:'flex'}}>
+        <div style={{ margin: '20px', fontSize: '0.8rem',fontWeight:'normal',paddingTop:'20px' }}>
+            <h1>Search the 7,000+ courses in Coursera Plus</h1>
+        </div>
+        <div style={{display:'flex',flexDirection:'row'}}>
+        <input type="text" placeholder="Arts and Humanities" style={{width:'1000px',backgroundColor:'white',border: '1px solid #ccc',marginLeft:'1px',borderRight:'none'}} />
+        <button style={{height:'50px',width:'50px',backgroundColor:'white',border: '1px solid #ccc',borderLeft:'none',paddingRight:'40px'}}>
+          <IoIosSearch color='black' style={{height:'30px',width:'30px',paddingRight:'20px'}} />
+        </button>
+        </div>
+        <div style={{display:'flex',flexDirection:'row',marginLeft:'50px',marginRight:'20px'}}>
+        {fields.map((field, index) => (
+            <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '30px' }}>
+                <button style={{backgroundColor:'#ECEFEF',color:'blue',borderWidth: '1px',
+  borderRadius: '30%'}}>{field}</button>
+            </div>
+        ))}
         </div>
     </div>
     </>

92efbbab58ed8a4a3b5c095a7f88c3221d5640a3
Aryan Gupta <30aryang@gmail.com>
2024-03-30 11:38:20 +0530

    Article and Page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Home from "./pages/Home.js"
 import Degrees from './pages/Degrees.js'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Articles from './pages/Articles.js';
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/degrees" element={<Degrees />} />
+          <Route path="/articles" element={<Articles />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Articles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Articles() {
+  return (
+    <div>
+        <div>
+            <h1>Coursera Articles</h1>
+            <p>We envision a world where anyone, anywhere has the power to transform their lives through learning. Take the next step in your learning journey by exploring these popular topics.</p>
+        </div>
+    </div>
+  )
+}

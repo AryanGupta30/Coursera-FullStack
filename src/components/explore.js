35ea5f36477fd8b9baddeca497bbfa90cd9796f3
Akshavya2509 <Akshavya2509@gmail.com>
2024-04-01 19:32:29 +0530

    explore

--- src/components/explore.js
+++ src/components/explore.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import '../css/explore.css';
-import {courses} from '../constants/data'
+import React from "react";
+import "../css/explore.css";
+import { courses } from "../constants/data";
 
 function App() {
   return (
     <div className="app">
-      <h1 style={{margin: '60px', fontSize: '2.7rem'}}>Explore Coursera</h1>
+      <h1 style={{ margin: "60px", fontSize: "2.7rem" }}>Explore Coursera</h1>
       <div className="courses">
         {courses.map((course, index) => (
           <div key={index} className="course-card">
             <h2>{course.category}</h2>
-            <p>{course.count}courses</p>
+            <p>{course.count} courses</p>
           </div>
         ))}
       </div>
